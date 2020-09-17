15d617286503e2416f5f62a672616b4791f2fa31
Mayank <mohitmv0673@gmail.com>
2020-09-18 00:36:18 +0530

    Tic-Tac-Toe

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IconComponent } from './components/icon/icon.component';
+//font-awesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+//tostr
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IconComponent,
+
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
